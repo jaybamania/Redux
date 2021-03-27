f4df599ec6c7ec0df46875ea8611a6e71367d4ec
jaybamania <jaykumardiu@gmail.com>
2021-03-27 16:43:27 +0530

    ReduxFiles

--- index.js
+++ index.js
@@ -27,3 +27,10 @@ const reducer = (state = initialState, action) =>{
     }
 }
 
+const store = createStore(reducer)
+console.log('Initial State ',store.getState())
+const unsubscribe = store.subscribe(()=> console.log('Updated State',store.getState()))
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+unsubscribe()
